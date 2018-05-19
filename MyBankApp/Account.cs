7acb6e3d5d7b011c510eca4df16608e71cd348e9
Carla Bosco <carlabosco@gmail.com>
2018-05-19 14:26:52 -0700

    Initial commit

---
+++ MyBankApp/Account.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBankApp
+{
+    #region Properties
+    class Account
+    {
+        public int AccountNumber { get; private set; }
+        public string EmailAddress { get; set; }
+        public decimal Balance { get; private set; }
+        public string TypeOfAccount { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        #region Methods
+        /// <summary>
+        /// Deposit money into account
+        /// </summary>
+        /// <param name="amount">Amount to deposit</param>
+        /// <returns>Updated balance</returns>
+        public decimal Deposit(decimal amount)
+        {
+            Balance += amount;
+            return Balance;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+
+        #endregion
+    }
+    #endregion
+}
---
+++ MyBankApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //instantiation
+            var account = new Account();
+            //account.AccountNumber = 12345;    user cannot set account number
+            account.EmailAddress = "test@test.com";
+            account.TypeOfAccount = "Checking";
+            //account.Balance = 10000000; user cannot set balance
+        }
+    }
+}
